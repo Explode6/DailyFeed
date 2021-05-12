ac13459d05f0f48767b4d1c461478e5afa3d1d75
VonPangpang <1061695123@qq.com>
2021-05-12 18:52:43 +0800

    数据库小改
    删除源后再次添加时，曾经被收藏的文章的重新链接

--- rssreader/app/src/main/java/com/example/rssreader/model/datamodel/DataBaseHelper.java
+++ rssreader/app/src/main/java/com/example/rssreader/model/datamodel/DataBaseHelper.java
@@ -59,7 +59,12 @@ public class DataBaseHelper {
             List<ArticleBrief> articleBriefs = LitePal.where("link = ?", articleBrief.getLink())
                     .find(ArticleBrief.class);
             if(!articleBriefs.isEmpty()){
-                //文章已存在, 直接返回
+                //文章已存在, 如果是该源曾经被收藏的文章，重新链接上外键
+                if(articleBriefs.get(0).getCollect() && articleBriefs.get(0).getChannel_id() == -1){
+                    articleBriefs.get(0).setChannel_id(channels.get(0).getId());
+                    articleBriefs.get(0).setPubTime((int)System.currentTimeMillis()/(60*60*24*1000));
+                    articleBriefs.get(0).update(articleBrief.getId());
+                }
                 return;
             }else {
                 //文章不存在, 添加
